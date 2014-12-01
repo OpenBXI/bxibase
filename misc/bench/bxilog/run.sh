5c3686193f290220c8908f153e28b318ddb80fde
Pierre Vignéras <pierre.vigneras@atos.net>
2014-12-01 16:10:03 +0000

    Improve the run script.

--- misc/bench/bxilog/run.sh
+++ misc/bench/bxilog/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if test $# -ne 1; then echo "Usage: $(basename $0) bench_cmd"
+  exit 1
+fi
 
-gcc -o bxilog_bench bxilog_bench.c -O3 -lpthread -lzmq -lbxi -std=c99 -D_POSIX_C_SOURCE=200809L -D_GNU_SOURCE
-rm -f /tmp/bxilog_bench.log;sync;/usr/bin/time -v ./bxilog_bench 5 60;wc -l /tmp/bxilog_bench.log;ls -l /tmp/bxilog_bench.log
+if test $1 == "bxilog_bench";then
+  gcc -o bxilog_bench bxilog_bench.c -O3 -lpthread -lzmq -lbxi -std=c99 -D_POSIX_C_SOURCE=200809L -D_GNU_SOURCE
+fi
+
+output=/tmp/$1.log
+
+rm -f $output;sync;/usr/bin/time -v $1 5 60;wc -l $output;ls -l $output
