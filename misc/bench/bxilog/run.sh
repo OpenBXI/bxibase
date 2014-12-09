43e9e85f523f45f76d432f6adb09ca6a76f82a01
Pierre Vignéras <pierre.vigneras@atos.net>
2014-12-09 10:40:49 +0000

    Enhance the bench

--- misc/bench/bxilog/run.sh
+++ misc/bench/bxilog/run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if test $# -ne 1; then echo "Usage: $(basename $0) bench_cmd"
+if test $# -ne 3; then echo "Usage: $(basename $0) bench_cmd threads duration"
   exit 1
 fi
 
 if test $1 == "bxilog_bench";then
-  gcc -o bxilog_bench bxilog_bench.c -O3 -lpthread -lzmq -lbxi -std=c99 -D_POSIX_C_SOURCE=200809L -D_GNU_SOURCE
+  gcc -o bxilog_bench bxilog_bench.c -O2 -lpthread -lzmq -lbxi -std=c99 -D_POSIX_C_SOURCE=200809L -D_GNU_SOURCE
 fi
 
+bench=$1
+threads=$2
+duration=$3
 output=/tmp/$1.log
 
-rm -f $output;sync;/usr/bin/time -v $1 5 60;wc -l $output;ls -l $output
+
+rm -f $output
+sync
+./$1 $threads $duration 
+
+echo -e "$bench\t$threads\t$duration\t$(cat $output | wc -l)\t$(du $output|cut -f 1)" >> /tmp/$bench.bench
