e64f362af63101bd69157e60152d6048247b12af
Jean-Noël Quintin <jean-noel.quitin@atos.net>
2015-09-22 13:21:19 +0000

    Use result file without specific name

--- tests/unit_t.c
+++ tests/unit_t.c
@@ -216,7 +216,7 @@ int main(int argc, char * argv[]) {
     }
 
     /* Run all tests using the automated interface */
-    CU_set_output_filename("./report/bxibase");
+    CU_set_output_filename("./report/cunit");
     CU_automated_run_tests();
     CU_list_tests_to_file();
 
