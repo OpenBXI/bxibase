9533cca53aa5cef9881cf7a14ff0f88de46b3791
Pierre Vignéras <pierre.vigneras@atos.net>
2016-01-22 10:22:48 +0000

    Support syslog in configuration file.

---
+++ packaged/lib/bxi/base/log/syslog_handler.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+"""
+@file syslog_handler.py bxilog syslog handler
+@authors Pierre Vignéras <pierre.vigneras@bull.net>
+@copyright 2013  Bull S.A.S.  -  All rights reserved.\n
+           This is not Free or Open Source software.\n
+           Please contact Bull SAS for details about its license.\n
+           Bull - Rue Jean Jaurès - B.P. 68 - 78340 Les Clayes-sous-Bois
+@namespace bxi.base.log.syslog_handler bxilog syslog handler
+
+"""
+
+import os
+import sys
+import syslog
+
+import bxi.ffi as bxiffi
+import bxi.base.err as bxierr
+import bxi.base as bxibase
+import bxi.base.log.filter as bxilogfilter
+
+# Find the C library
+__FFI__ = bxiffi.get_ffi()
+__BXIBASE_CAPI__ = bxibase.get_capi()
+
+
+def add_handler(configobj, section_name, c_config):
+    """
+    Add a syslog handler configured from the given section in configobj to the c_config
+
+    @param[in] configobj the configobj (a dict) representing the whole configuration
+    @param[in] section_name the section name in the configobj that must be used
+    @param[inout] c_config the bxilog configuration where the handler must be added to 
+    """
+    section = configobj[section_name]
+    filters_str = section['filters']
+    facility_str = section['facility']
+
+    filters = bxilogfilter.parse_filters(filters_str)
+    facility = __FFI__.cast('int', eval('syslog.%s' % facility_str))
+
+    identity = __FFI__.new('char[]', sys.argv[0])
+    option = __FFI__.cast('int', syslog.LOG_PID)
+    __BXIBASE_CAPI__.bxilog_config_add_handler(c_config,
+                                               __BXIBASE_CAPI__.BXILOG_SYSLOG_HANDLER,
+                                               filters,
+                                               identity,
+                                               option,
+                                               facility)
