4af4915e0475164eda7421bbebde292c3ab50300
Sébastien Miquée <sebastien.miquee@atos.net>
2016-08-09 10:13:53 +0200

    Adding the Python wrapper for the remote handler

---
+++ packaged/lib/bxi/base/log/remote_handler.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+@file remote_handler.py bxilog remote handler
+@authors Sébastien Miquée <sebastien.miquee@atos.net>
+@copyright 2016  Bull S.A.S.  -  All rights reserved.\n
+           This is not Free or Open Source software.\n
+           Please contact Bull SAS for details about its license.\n
+           Bull - Rue Jean Jaurès - B.P. 68 - 78340 Les Clayes-sous-Bois
+@namespace bxi.base.log.remote_handler bxilog remote handler
+
+"""
+
+import bxi.ffi as bxiffi
+import bxi.base as bxibase
+
+
+# Find the C library
+__FFI__ = bxiffi.get_ffi()
+__BXIBASE_CAPI__ = bxibase.get_capi()
+
+
+def add_handler(configobj, section_name, c_config):
+    """
+    Add a remote handler configured from the given section in configobj to the c_config
+
+    @param[in] configobj the configobj (a dict) representing the whole configuration
+    @param[in] section_name the section name in the configobj that must be used
+    @param[inout] c_config the bxilog configuration where the handler must be added to
+    """
+    section = configobj[section_name]
+    url = section['url']
+
+    __BXIBASE_CAPI__.bxilog_config_add_handler(c_config,
+                                               __BXIBASE_CAPI__.BXILOG_REMOTE_HANDLER,
+                                               url)
