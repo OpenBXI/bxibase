e8a0575198eba888486f337bc36fc44a60f260a0
Pierre Vignéras <pierre.vigneras@gmail.com>
2016-12-29 19:47:55 +0100

    Refactoring move level stuff to level.[hc]

---
+++ packaged/src/log/level.c
@@ -0,0 +1,137 @@
+/* -*- coding: utf-8 -*-
+ ###############################################################################
+ # Author: Pierre Vigneras <pierre.vigneras@bull.net>
+ # Created on: May 24, 2013
+ # Contributors:
+ ###############################################################################
+ # Copyright (C) 2012  Bull S. A. S.  -  All rights reserved
+ # Bull, Rue Jean Jaures, B.P.68, 78340, Les Clayes-sous-Bois
+ # This is not Free or Open Source software.
+ # Please contact Bull S. A. S. for details about its license.
+ ###############################################################################
+ */
+
+
+
+#include <string.h>
+
+#include "bxi/base/str.h"
+#include "bxi/base/err.h"
+
+#include "bxi/base/log.h"
+#include "bxi/base/log/level.h"
+
+
+//*********************************************************************************
+//********************************** Defines **************************************
+//*********************************************************************************
+
+//*********************************************************************************
+//********************************** Types ****************************************
+//*********************************************************************************
+
+//*********************************************************************************
+//********************************** Static Functions  ****************************
+//*********************************************************************************
+//--------------------------------- Generic Helpers --------------------------------
+
+//*********************************************************************************
+//********************************** Global Variables  ****************************
+//*********************************************************************************
+
+// The internal logger
+SET_LOGGER(LOGGER, BXILOG_LIB_PREFIX "bxilog.lvl");
+
+
+static char * _LOG_LEVEL_NAMES[] = { "off",
+                                     "panic",
+                                     "alert",
+                                     "critical",
+                                     "error",
+                                     "warning",
+                                     "notice",
+                                     "output",
+                                     "info",
+                                     "debug",
+                                     "fine",
+                                     "trace",
+                                     "lowest"
+};
+
+//*********************************************************************************
+//********************************** Implementation    ****************************
+//*********************************************************************************
+
+
+bxierr_p bxilog_get_level_from_str(char * level_str, bxilog_level_e *level) {
+    if (0 == strcasecmp("off", level_str)) {
+        *level = BXILOG_OFF;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("panic", level_str)
+            || 0 == strcasecmp("emergency", level_str)) {
+        *level = BXILOG_PANIC;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("alert", level_str)) {
+        *level = BXILOG_ALERT;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("critical", level_str)
+            || 0 == strcasecmp("crit", level_str)) {
+        *level = BXILOG_CRIT;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("error", level_str)
+            || 0 == strcasecmp("err", level_str)) {
+        *level = BXILOG_ERR;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("warning", level_str)
+            || 0 == strcasecmp("warn", level_str)) {
+        *level = BXILOG_WARN;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("notice", level_str)) {
+        *level = BXILOG_NOTICE;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("output", level_str)
+            || 0 == strcasecmp("out", level_str)) {
+        *level = BXILOG_OUTPUT;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("info", level_str)) {
+        *level = BXILOG_INFO;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("debug", level_str)) {
+        *level = BXILOG_DEBUG;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("fine", level_str)) {
+        *level = BXILOG_FINE;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("trace", level_str)) {
+        *level = BXILOG_TRACE;
+        return BXIERR_OK;
+    }
+    if (0 == strcasecmp("lowest", level_str) || 0 == strcasecmp("all", level_str)) {
+        *level = BXILOG_LOWEST;
+        return BXIERR_OK;
+    }
+    *level = BXILOG_LOWEST;
+    return bxierr_gen("Bad log level name: %s", level_str);
+}
+
+size_t bxilog_get_all_level_names(char *** names) {
+    *names = _LOG_LEVEL_NAMES;
+    return ARRAYLEN(_LOG_LEVEL_NAMES);
+}
+
+
+//*********************************************************************************
+//********************************** Static Helpers Implementation ****************
+//*********************************************************************************
+
