ed074442a05f1dd0d65e520b22df27fd4cd998ae
Pierre Vignéras <pierre.vigneras@gmail.com>
2017-03-18 18:47:20 +0100

    Fix travis file

--- misc/travis/install-dependencies.sh
+++ misc/travis/install-dependencies.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 TMPDIR=${TMPDIR:-/tmp}
-
-cat /proc/cpuinfo
+CPUS_NB=$(($(grep -c ^processor /proc/cpuinfo)+1))
 
 wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.gz && \
     tar xvf zeromq-* -C ${TMPDIR} && \
     cd ${TMPDIR}/zeromq* && \
     ./configure && \
-    make && \
-    sudo make install && \
+    make -j${CPUS_NB} && \
+    sudo make -j${CPUS_NB} install && \
     wget https://github.com/OpenBXI/backtrace/releases/download/1.2.0-Bull.6.0/backtrace-1.2.0.tar.gz && \
     tar xvf backtrace-1.2.0.tar.gz -C ${TMPDIR} && \
     /tmp/backtrace-1.2.0/configure  && \
     make  && \
-    sudo make install && \
+    sudo make -j${CPUS_NB} install && \
     sudo ldconfig
 
