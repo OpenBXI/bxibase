831d100a90df88d2003b956f88958dff1c726de2
Pierre Vignéras <pierre.vigneras@gmail.com>
2017-03-18 17:38:08 +0100

    Fix typo

--- misc/travis/install-dependencies.sh
+++ misc/travis/install-dependencies.sh
@@ -3,4 +3,4 @@
 wget https://github.com/OpenBXI/backtrace/archive/1.2.0-Bull.6.0.tar.gz
 tar xvf 1.2.0-Bull.6.0.tar.gz
 cd backtrace*
-./boostrap.sh;./configure;make -j;make install
+./bootstrap.sh;./configure;make -j;make install
