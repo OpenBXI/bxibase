9bd1348b14fa4848652f8450667671d4d0b34ee0
Pierre Vignéras <pierre.vigneras@gmail.com>
2017-03-18 17:47:51 +0100

    Enhance travis file

--- misc/travis/install-dependencies.sh
+++ misc/travis/install-dependencies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-wget https://github.com/OpenBXI/backtrace/archive/1.2.0-Bull.6.0.tar.gz
-tar xvf 1.2.0-Bull.6.0.tar.gz
-cd backtrace*
-./bootstrap.sh;./configure;make -j;make install
+BUILD_DIR=${TMPDIR}/build
+wget https://github.com/OpenBXI/backtrace/archive/1.2.0-Bull.6.0.tar.gz && \
+tar xvf 1.2.0-Bull.6.0.tar.gz -C ${TMPDIR} && \
+mkdir -p ${BUILD_DIR} && \
+cd ${BUILD_DIR}; && \
+/tmp/backtrace*/bootstrap.sh && \
+/tmp/backtrace*/configure  && \
+make -j && \
+make install
