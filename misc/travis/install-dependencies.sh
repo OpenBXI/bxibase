74d4018642ce9069cdf999566d64c83216c3c2fd
Pierre Vignéras <pierre.vigneras@gmail.com>
2017-03-19 10:33:18 +0100

    Try to understand why cffi does not work

--- misc/travis/install-dependencies.sh
+++ misc/travis/install-dependencies.sh
@@ -5,6 +5,7 @@ CPUS_NB=$(($(grep -c ^processor /proc/cpuinfo)+1))
 
 python -c 'import cffi'
 
+exit 1
 wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.gz && \
     tar xvf zeromq-* -C ${TMPDIR} && \
     cd ${TMPDIR}/zeromq* && \
