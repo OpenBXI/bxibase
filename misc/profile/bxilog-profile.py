34cce58a13ca48b21256ce3eeaa0fd8d418794a6
Pierre Vignéras <pierre.vigneras@gmail.com>
2017-04-09 00:11:05 +0200

    Provide a simple program for profiling

---
+++ misc/profile/bxilog-profile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#import bxi.base.log as bxilog
+import logging as bxilog
+
+import sys
+
+# bxilog.set_config({'handlers':'file',
+#                    'file':{'module': 'bxi.base.log.file_handler',
+#                            'path': '/tmp/bxilog-profile.bxilog',
+#                            'append': False,
+#                            'filters': ':out',
+#                           },
+#                   })
+bxilog.basicConfig(level=bxilog.WARNING)
+
+logger = bxilog.getLogger('foo.bar')
+
+for i in xrange(int(sys.argv[1])):
+    
+    logger.warning("Something to say?")
+
